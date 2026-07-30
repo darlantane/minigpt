b413a97ac35d57d1e68f7829a59acf19c6befa9e
darlantane <darlan.tane@gmail.com>
2026-07-30 22:06:20 +0200

    embending

--- embending.py
+++ embending.py
@@ -14,3 +14,20 @@ class TokenEmbedding(nn.Module):
 
     def forward(self, idx):
         return self.embedding(idx)
+    
+
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, block_size, n_embed):
+        super().__init__()
+
+        self.embedding = nn.Embedding(
+            block_size,
+            n_embed
+        )
+
+    def forward(self, T):
+
+        positions = torch.arange(T)
+
+        return self.embedding(positions)
